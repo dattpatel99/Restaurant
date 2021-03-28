69aef4deeb9c09044ce6de6fda20c95097e84d16
DarkMist9991 <dhpatel1999@gmail.com>
2021-03-29 00:27:27 +0530

    Working On
    Making order only possible if logged in otherwise asking user to login. Showing menu items.

--- restaurant/views.py
+++ restaurant/views.py
@@ -8,7 +8,6 @@ from datetime import datetime
 from django.contrib.auth import authenticate, login as dj_login, logout as dj_logout
 
 # Create your views here.
-
 def home(request):
    
     return render(request, 'home.html')
@@ -20,7 +19,13 @@ def menu(request):
     return render(request, 'menu.html')
 
 def order(request):
-    # IDK what it does pretty much copied from: CodeWithHarry
+
+    # in the event the usre is smart and enter /order/ in the url section
+    if not request.user.is_authenticated:
+        messages.info(request, "Please login or signup before trying to order.")
+        return render(request, "login.html")
+
+    # Learned from: CodeWithHarry
     # Credit: https://www.youtube.com/watch?v=6iDW97emfB0&list=PL1PPKISJVChyleKhFnRBWeYvBGElatze-&index=15&t=31s
     
     allItems = []
@@ -51,7 +56,7 @@ def order(request):
         # adds its to all items
         # Creates a list inside of the list allItems. The inner list holds: A query set of food type Item, followed by a range and then number of slide 
         allItems.append([food, range(1, nSlides), nSlides])
-    
+        
     context = {"allItems": allItems}
     
     return render(request, 'order.html', context)
@@ -64,21 +69,29 @@ Works
 Logs in user, if user entry wrong outputs error message
 '''
 def login(request):
+    
     error = ""
+    
     if request.method == "POST":
         # Gets users input for each field
         userMail = request.POST.get('inputedEmail')
         userPass = request.POST.get('inputedPassword')
         user = authenticate(request, username=userMail, password=userPass)
         # if there is a user of such then logs them in and renders the home page
+    
         if user is not None:
             dj_login(request, user)
             error = "no"
             return render(request, "home.html")
+        # Using elif because when redirecting from /order then message is show cased twice
         # otherwise changes the error var to yes
-        else:
+    
+        elif user is None:
             error = "yes"
-    context={'error':error}
+            messages.warning(request, "Invalid username or password. Try again.")
+    
+    context = {'error': error}
+
     return render(request, "login.html", context)
 
 
@@ -110,3 +123,35 @@ Logs out user and sends them to login page
 def logout(request):
     dj_logout(request)
     return render(request, "login.html")
+
+
+'''
+allItems = []
+    # Creates a list of dictionaries with catergory and itemName as key and the cats and names as value
+    categoriesOfProducts = Item.objects.values('category', 'itemName')
+    
+    # Creates a set 
+    # From the previous list takes only categories and creates a set of only categories
+    categories = {i['category'] for i in categoriesOfProducts}
+
+    # loops through the categories set and selects each category
+    for cat in categories:
+        # Filters the food item based on the category
+        food = Item.objects.filter(category = cat)
+
+        # Gets the number of food items in that category
+        numItems = len(food)
+
+        # creates nSlides for that food item
+        if (numItems > 3):
+            if (numItems // 3 == 0):
+                nSlides = numItems / 3
+            else:
+                nSlides = ceil(numItems / 3)
+        else:
+            nSlides = 1
+
+        # adds its to all items
+        # Creates a list inside of the list allItems. The inner list holds: A query set of food type Item, followed by a range and then number of slide 
+        allItems.append([food, range(1, nSlides), nSlides])
+'''
